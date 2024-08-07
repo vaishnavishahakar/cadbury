d572d6acec1a285687343a36fc8c3b77fc80f90f
vaishnavishahakar <vaishnavishahakar1@gmail.com>
2024-08-07 20:19:41 +0530

    add node.js file

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<h1>cadbury 🍫</h1>)
+
+//this method also be used
+// import ReactDom from "react-dom/client";
+
+// const root = ReactDom.createRoot(document.getElementById("root"));
+
+// root.render(<h1>cadbury 🍫</h1>)
